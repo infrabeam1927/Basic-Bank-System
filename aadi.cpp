e88274e6fcd77d730304b23ba572196cea934b5d
Aaditya Senthilkumar <75241997+infrabeam1927@users.noreply.github.com>
2023-12-30 19:13:15 +0000

    second commit

--- aadi.cpp
+++ aadi.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 using namespace std;
-
+double calc(double balance,double amount );
 int main() {
-  int x = 100 + 50;
-  cin>>x;
-  cout << x+40<<endl<<"aa";
-  return 0;
+  int choice;
+  double amount;
+  cout << "\n1. Calculate" << endl;
+  cout << "2. Withdraw" << endl;
+  cout << "3. Display Account" << endl;
+  cout << "4. Exit" << endl;
+  cout << "Enter choice: ";
+  cin >> choice;
+  do
+  {
+    switch (choice)
+    {
+    case 1:
+      cout<<"how much money do you want to add or subtract";
+      cin>>amount;
+      calc(100,amount);
+      break;
+    
+    default:
+      cout<<"default case";
+      break;
+    }
+    /* code */
+  } while (choice!=5); 
 }
 
+double calc(double balance,double amount ){
+  cout<<balance+amount;
+  return balance+amount;
+
+}
