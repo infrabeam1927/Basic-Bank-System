b3d0c054f986b57746ea35235aab0b3cc17f9540
Aaditya Senthilkumar <75241997+infrabeam1927@users.noreply.github.com>
2023-12-31 17:11:28 +0000

    4th commit

--- aadi.cpp
+++ aadi.cpp
@@ -1,54 +1,79 @@
 #include <iostream>
-#include<fstream>
+#include <fstream>
 using namespace std;
-double calc(double balance,double amount );
-class account{
-  int acno;
-  char name;
-  double balance;
+
+double calc(double balance, double amount);
+
+class account {
 public:
-  double balancefinder();
-  void writeToFile(const account& account) {
+    int acno;
+    string name; // Change from char to string
+    double balance;
+    double balancefinder();
+    void writeToFile(account a) {
+        ofstream outFile("data.txt", ios::app);
+        if (outFile.is_open()) {
+            outFile << a.acno << " " << a.name << " " << a.balance << endl;
+            cout << "Account information written to data.txt successfully." << endl;
+            outFile.close();
+        } else {
+            cerr << "Error opening data.txt for writing." << endl;
+        }
+    }
+};
+
+void writeToFile(account a);
+
+void writeToFile(account a) {
     ofstream outFile("data.txt", ios::app);
     if (outFile.is_open()) {
-        outFile << account.acno << " " << account.name << " " << account.balance << endl;
+        outFile << a.acno << " " << a.name << " " << a.balance << endl;
         cout << "Account information written to data.txt successfully." << endl;
         outFile.close();
     } else {
         cerr << "Error opening data.txt for writing." << endl;
-        }
-    }  
-};
-int main() {
-  int choice;
-  double amount;
-  cout << "\n1. Calculate/calculator" << endl;
-  cout << "2. Withdraw" << endl;
-  cout << "3. Withdraw" << endl;
-  cout << "4. Display Account" << endl;
-  cout << "5. Exit" << endl;
-  cout << "Enter choice: ";
-  cin >> choice;
-  do
-  {
-    switch (choice)
-    {
-    case 1:
-      cout<<"how much money do you want to add or subtract";
-      cin>>amount;
-      calc(100,amount);
-      break;
-    
-    default:
-      cout<<"default case";
-      break;
     }
-    /* code */
-  } while (choice!=5); 
 }
 
-double calc(double balance,double amount ){
-  cout<<balance+amount;
-  return balance+amount;
+int main() {
+    int choice, acno;
+    account a;
+    double amount;
+    do {
+        cout << "\n1. Calculate/calculator" << endl;
+        cout << "2. Create account" << endl;
+        cout << "3. Withdraw" << endl;
+        cout << "4. Display Account" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
 
+        switch (choice) {
+        case 1:
+            cout << "how much money do you want to add or subtract: ";
+            cin >> amount;
+            calc(100, amount);
+            break;
+        case 2:
+            cout << "create a new account enter acno: ";
+            cin >> a.acno;
+            cout << "enter name: ";
+            cin.ignore(); // Clear the input buffer
+            getline(cin, a.name); 
+            a.balance = 0;
+            writeToFile(a);
+            break;
+        default:
+            cout << "default case" << endl;
+            break;
+        }
+        /* code */
+    } while (choice != 5);
+
+    return 0;
+}
+
+double calc(double balance, double amount) {
+    cout << balance + amount;
+    return balance + amount;
 }
