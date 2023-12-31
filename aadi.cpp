0a622d8219ee1fbaee016fc9524597ba06ad4ac1
Aaditya Senthilkumar <75241997+infrabeam1927@users.noreply.github.com>
2023-12-31 17:10:56 +0000

    third commit

--- aadi.cpp
+++ aadi.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
+#include<fstream>
 using namespace std;
 double calc(double balance,double amount );
+class account{
+  int acno;
+  char name;
+  double balance;
+public:
+  double balancefinder();
+  void writeToFile(const account& account) {
+    ofstream outFile("data.txt", ios::app);
+    if (outFile.is_open()) {
+        outFile << account.acno << " " << account.name << " " << account.balance << endl;
+        cout << "Account information written to data.txt successfully." << endl;
+        outFile.close();
+    } else {
+        cerr << "Error opening data.txt for writing." << endl;
+        }
+    }  
+};
 int main() {
   int choice;
   double amount;
-  cout << "\n1. Calculate" << endl;
+  cout << "\n1. Calculate/calculator" << endl;
   cout << "2. Withdraw" << endl;
-  cout << "3. Display Account" << endl;
-  cout << "4. Exit" << endl;
+  cout << "3. Withdraw" << endl;
+  cout << "4. Display Account" << endl;
+  cout << "5. Exit" << endl;
   cout << "Enter choice: ";
   cin >> choice;
   do
