84c76ea77059bcec7b3f1883a1aa5ebbd173f012
Aaditya Senthilkumar <75241997+infrabeam1927@users.noreply.github.com>
2024-01-01 09:13:37 +0000

    Fifth Commit

--- aadi.cpp
+++ aadi.cpp
@@ -9,17 +9,6 @@ public:
     int acno;
     string name; // Change from char to string
     double balance;
-    double balancefinder();
-    void writeToFile(account a) {
-        ofstream outFile("data.txt", ios::app);
-        if (outFile.is_open()) {
-            outFile << a.acno << " " << a.name << " " << a.balance << endl;
-            cout << "Account information written to data.txt successfully." << endl;
-            outFile.close();
-        } else {
-            cerr << "Error opening data.txt for writing." << endl;
-        }
-    }
 };
 
 void writeToFile(account a);
@@ -35,6 +24,31 @@ void writeToFile(account a) {
     }
 }
 
+bool checkAccountExists(int targetAcno) {
+    ifstream inFile("data.txt");
+    if (inFile.is_open()) {
+        int acno;
+        string fname;
+        string lname;
+        double balance;
+
+        while (inFile >> acno >> fname >> lname >> balance) {
+            if (acno == targetAcno) {
+                // Account found
+                inFile.close();
+                return true;
+            }
+        }
+
+        // Account not found
+        inFile.close();
+        return false; // Indicate that the account doesn't exist
+    } else {
+        cerr << "Error opening data.txt for reading." << endl;
+        return false; // Indicate an error
+    }
+}
+
 int main() {
     int choice, acno;
     account a;
@@ -44,7 +58,8 @@ int main() {
         cout << "2. Create account" << endl;
         cout << "3. Withdraw" << endl;
         cout << "4. Display Account" << endl;
-        cout << "5. Exit" << endl;
+        cout << "5. Check Account Exists and Get Balance" << endl;
+        cout << "6. Exit" << endl;
         cout << "Enter choice: ";
         cin >> choice;
 
@@ -55,13 +70,21 @@ int main() {
             calc(100, amount);
             break;
         case 2:
-            cout << "create a new account enter acno: ";
-            cin >> a.acno;
-            cout << "enter name: ";
-            cin.ignore(); // Clear the input buffer
-            getline(cin, a.name); 
-            a.balance = 0;
-            writeToFile(a);
+           cout << "create a new account enter acno: ";
+           cin >> a.acno;
+           if (checkAccountExists(a.acno)) {
+            cout << "Account exists." << endl;
+            break; // Return to the initial choices
+           }
+           cout << "enter name: ";
+           cin.ignore(); // Clear the input buffer
+           getline(cin, a.fname);
+
+           a.balance = 0;
+           writeToFile(a);
+           break;
+
+        case 5:
             break;
         default:
             cout << "default case" << endl;
